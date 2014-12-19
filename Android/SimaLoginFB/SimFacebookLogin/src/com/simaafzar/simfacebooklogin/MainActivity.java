a53d5fe64d8e83eb8034d5ed442fc84366d3278e
Ghassem <ghassem+github@gmail.com>
2014-12-18 22:23:31 -0500

    Facebook Login Application
    Sima Facebook Login uses Facebook account for authentication.

---
+++ Android/SimaLoginFB/SimFacebookLogin/src/com/simaafzar/simfacebooklogin/MainActivity.java
@@ -0,0 +1,70 @@
+package com.simaafzar.simfacebooklogin;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.AppEventsLogger;
+
+
+public class MainActivity extends FragmentActivity {
+	private static final String TAG = "MainActivity";
+	private MainFragment mainFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+            .beginTransaction()
+            .add(android.R.id.content, mainFragment)
+            .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+            .findFragmentById(android.R.id.content);
+        }
+        
+        
+    }
+    
+    @Override
+    protected void onResume() {
+      super.onResume();
+
+      // Logs 'install' and 'app activate' App Events.
+      //AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+      super.onPause();
+
+  
+      // Logs 'app deactivate' App Event.
+     //AppEventsLogger.deactivateApp(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
